1f2324a380163f4acd99204d2d8eae67d594f9c5
Matheus-dSM <163189960+Matheus-dSM@users.noreply.github.com>
2024-04-07 18:13:14 -0300

    Added url-vsc.h Maybe later make so it has less hardcoded things.

---
+++ url-vsc.h
@@ -0,0 +1,6 @@
+//TODO Maybe make these not hardcoded?
+#define URL "https://github.com/VSCodium/vscodium/releases/latest/"
+#define FILE_END "-src.tar.gz"
+#define SHA_END ".sha256"
+#define PATH_SAVE "/tmp/vscu/"
+
