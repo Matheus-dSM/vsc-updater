eab382df0edea5fa221aea79daa06806d0bdf4c3
Matheus-dSM <163189960+Matheus-dSM@users.noreply.github.com>
2024-04-07 18:11:32 -0300

    Added vscu.c in basic yet working state.

---
+++ vscu.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdlib.h>
+#include "download.h"
+
+int main(void){
+    int i = getfile();
+    if(i = 0){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
