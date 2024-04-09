0a48d465b812a1b05174a438455266cc0ed2a86e
Matheus-dSM <163189960+Matheus-dSM@users.noreply.github.com>
2024-04-09 00:36:44 -0300

    Added bool flags and header for them. Added a few other defines.

---
+++ global-vsc.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+//TODO Maybe make these not hardcoded?
+#define URL "https://github.com/VSCodium/vscodium/releases/latest/"
+#define FILE_END "-src.tar.gz"
+#define SHA_END ".sha256"
+#define D_NAME "vscu"
+#define TD_NAME "/tmp"
+#define VERSION "1.0.0"//Just an example
+
+extern bool vflag;
+extern bool dflag;
+
