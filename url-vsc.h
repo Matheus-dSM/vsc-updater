2690732b4f70b41b7f0df5c5a00b404c17693d69
Matheus-dSM <163189960+Matheus-dSM@users.noreply.github.com>
2024-04-09 00:40:25 -0300

    Removed(actually renamed) url-vsc.h --> global-vsc.h

--- url-vsc.h
+++
@@ -1,7 +0,0 @@
-//TODO Maybe make these not hardcoded?
-#define URL "https://github.com/VSCodium/vscodium/releases/latest/"
-#define FILE_END "-src.tar.gz"
-#define SHA_END ".sha256"
-#define D_NAME "vscu"
-#define TD_NAME "/tmp"
-
