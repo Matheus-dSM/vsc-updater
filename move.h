906f14617f60a4840c17fbf87f3fe54329e41c76
Matheus-dSM <163189960+Matheus-dSM@users.noreply.github.com>
2024-04-09 00:38:20 -0300

    Added the basic template for move.h

---
+++ move.h
@@ -0,0 +1,10 @@
+#include <string.h>//Strings
+#include <stdlib.h>//Misc, mainly malloc
+#include <unistd.h>//For chdir
+#include <sys/stat.h>//For creating the dir
+#include <stdbool.h>//For bool for the -v flag check
+#include "global-vsc.h"
+
+int move(void){
+    //Getting the current directory
+}
